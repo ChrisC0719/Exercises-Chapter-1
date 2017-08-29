cbc714444e3fba4aac77fba217111f7dc3f43671
ChrisC0719 <31348437+ChrisC0719@users.noreply.github.com>
2017-08-29 09:27:10 -0500

    Folder Update

---
+++ Chapter1/src/exercises/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+package exercises;
+
+import javax.swing.JOptionPane;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	JOptionPane.showMessageDialog(null, "Favorite Movie Quote || Good morning, and in case I don't see ya good afternoon, good evening, and good night!" 
+			+" -The Truman Show");
+	JOptionPane.showMessageDialog(null, "We all end up dead! It's just a question of how and why.");
+	JOptionPane.showMessageDialog(null, "Movie Title: Braveheart || Character: William Wallace || Movie Came Out: 1995");}
+
+}
---
+++ Chapter1/src/exercises/TableAndChairs.java
@@ -0,0 +1,26 @@
+package exercises;
+
+public class TableAndChairs {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+	System.out.println("C");
+	System.out.println("C");
+	System.out.println("C");
+	System.out.println("C");
+	System.out.println("C");	
+	System.out.println("C");		
+	System.out.println("C");	
+	System.out.println("CCCCCCCCCCCCC");  
+	System.out.println("C           C");	
+	System.out.println("C           C");
+	System.out.println("C           C");
+	System.out.println("C           C");
+	System.out.println("C           C");
+	System.out.println("C           C");
+	
+	
+	}
+
+}
